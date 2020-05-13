ed57a0f59d52d370de280ed69aa820a0e4b713ee
harshit3012 <54054140+harshit3012@users.noreply.github.com>
2020-05-13 16:27:11 +0530

    Update lab2_banking_14022020.sql

--- lab2_banking_14022020.sql
+++ lab2_banking_14022020.sql
@@ -1,41 +1,98 @@
-CREATE DATABASE Banking_enterprise;
-USE Banking_enterprise;
-CREATE TABLE Branch(
-	branch_name VARCHAR(20) NOT NULL,
-	branch_city VARCHAR(20) NOT NULL,
-	assets REAL NOT NULL,
-	PRIMARY KEY(branch_name));
-    
-CREATE TABLE BankAccount(
-	accno int(13) NOT NULL UNIQUE,
-	branch_name VARCHAR(20) NOT NULL,
-	balance REAL NOT NULL,
-	PRIMARY KEY(accno),
-	FOREIGN KEY(branch_name) REFERENCES Branch(branch_name));
+create database bankingDB;
+use bankingDB;
 
-CREATE TABLE BankCustomer(
-	customer_name VARCHAR(40) NOT NULL UNIQUE,
-	customer_street VARCHAR(30) NOT NULL,
-	customer_city VARCHAR(20) NOT NULL,
-	PRIMARY KEY(customer_name));
+create table branch(
+branch_name varchar(30),
+branch_city varchar(30),
+assets REAL,
+primary key(branch_name));
 
-CREATE TABLE Depositer(
-	customer_name VARCHAR(40) NOT NULL,
-	accno int(13) NOT NULL UNIQUE,
-	PRIMARY KEY(customer_name, accno),
-	FOREIGN KEY(customer_name) REFERENCES BankCustomer(customer_name),
-	FOREIGN KEY(accno) REFERENCES BankAccount(accno));
+create table accounts(
+accno int,
+branch_name varchar(30),
+balance REAL,
+primary key(accno),
+foreign key(branch_name)references Branch(branch_name));
 
-CREATE TABLE Loan(
-	loan_number int(14) NOT NULL UNIQUE,
-	branch_name VARCHAR(20) NOT NULL,
-	amount REAL NOT NULL,
-	PRIMARY KEY(loan_number));
+create table depositor(
+cust_name varchar(30),
+accno int,
+primary key(cust_name,accno));
+alter table Depositor add foreign key(accno) references ACCOUNTS(accno);
 
-INSERT INTO branch(branch_name, branch_city, assets) VALUES ("SBI_Chamarajpet", "Bangalore", 50000);
-INSERT INTO branch(branch_name, branch_city, assets) VALUES ("SBI_ResidencyRoad", "Bangalore", 10000), ("SBI_ShivajiRoad", "Bombay", 20000), ("SBI_ParliamentRoad", "Delhi", 10000), ("SBI_JantarMantar", "Delhi", 20000);
-INSERT INTO bankaccount(accno, branch_name, balance) VALUES (1, "SBI_Chamarajpet", 2000), (2, "SBI_ResidencyRoad", 5000), (3, "SBI_ShivajiRoad", 6000), (4, "SBI_ParliamentRoad", 9000), (5, "SBI_JantarMantar", 8000), (6, "SBI_ShivajiRoad", 4000), (8, "SBI_ResidencyRoad", 4000), (9, "SBI_ParliamentRoad", 3000), (10, "SBI_ResidencyRoad", 5000), (11, "SBI_JantarMantar", 2000);
-INSERT INTO bankcustomer(customer_name, customer_street, customer_city) VALUES ("Avinash", "Bull_Temple_Road", "Bangalore"), ("Dinesh", "Bannergatta_Road", "Bangalore"), ("Mohan", "NationalCollege_Road", "Bangalore"), ("Nikil", "Akbar_Road", "Delhi"), ("Ravi", "Prithviraj_Road", "Delhi");
+create table bankcust(
+cust_name varchar(30),
+cust_street varchar(30),
+city varchar(30),
+primary key(cust_name));
 
-INSERT INTO depositer(customer_name, accno) VALUES ("Avinash", 1), ("Dinesh", 2), ("Nikil", 4), ("Ravi", 5);
-INSERT INTO loan(loan_number, branch_name, amount) VALUES (1, "SBI_Chamarajpet", 1000), (2, "SBI_ResidencyRoad", 2000), (3, "SBI_ShivajiRoad", 3000), (4, "SBI_ParliamentRoad", 4000), (5, "SBI_JantarMantar", 5000);
+
+create table loan(
+loan_num int,
+branch_name varchar(30),
+amount real,
+primary key(loan_num),
+foreign key(branch_name)references Branch(branch_name));
+
+create table borrower(
+cust_name varchar(20),
+loan_num int,
+foreign key(cust_name) references bankcust(cust_name),
+foreign key(loan_num) references loan(loan_num));
+
+
+insert into branch values ('SBI_Chamrajpet','Bangalore',50000),('SBI_ResidencyRoad','Bangalore',10000),
+('SBI_ShivajiRoad','Bombay',20000),('SBI_ParlimentRoad','Delhi',10000),('SBI_Jantarmantar','Delhi',20000);
+
+insert into ACCOUNTS values(1,'SBI_Chamrajpet',2000),(2,'SBI_ResidencyRoad',5000),(3,'SBI_ShivajiRoad',6000),
+(4,'SBI_ParlimentRoad',9000),(5,'SBI_Jantarmantar',8000),(6,'SBI_ShivajiRoad',4000),(7,'SBI_ResidencyRoad',4000),
+(8,'SBI_ResidencyRoad',4000),(9,'SBI_ResidencyRoad',3000),(10,'SBI_ResidencyRoad',5000),
+(11,'SBI_Jantarmantar',2000);
+
+insert into BankCust values('Avinash','Bull_temple_road','Bangalore'),('Dinesh','Bannergatta_road','Bangalore'),
+('Mohan','NationalCollege_road','Bangalore'),('Nikil','Akbar_road','Delhi'),('Ravi','Prithviraj_road','Delhi');
+
+insert into Depositor values('Avinash',1),('Dinesh',2),('Nikil',4),('Ravi',5),('Avinash',8),('Nikil',9),('Dinesh',10),
+('Nikil',11);
+
+insert into loan values(1,'SBI_Chamrajpet',1000),(2,'SBI_ResidencyRoad',2000),(3,'SBI_ShivajiRoad',3000),
+(4,'SBI_ParlimentRoad',4000),(5,'SBI_Jantarmantar',5000);
+
+insert into borrower values('Avinash',1), ('Dinesh',2), ('Nikil',3), ('Avinash', 4), ('Dinesh', 5);
+
+select C.cust_name 
+from BankCust C
+where exists (
+				select D.cust_name, count(D.cust_name)
+                from depositor D,accounts BA
+                where
+                D.accno = BA.accno AND C.cust_name = D.cust_name AND BA.branch_name='SBI_ResidencyRoad' 
+                group by D.cust_name
+                having count(D.cust_name) >=2
+                );
+                
+select BC.cust_name from BankCust BC
+where not exists(
+					select branch_name from branch where branch_city = 'Delhi' and branch_name
+                    not in
+                    (select BA.branch_name from depositor D , accounts BA
+                    where D.accno = BA.accno AND BC.cust_name = D.cust_name)
+                    );
+                    /*OR*/
+                    select cust_name, accno from depositor where accno in(
+    select accno from accounts where branch_name in(
+    select branch_name from branch where branch_city='Delhi'));
+
+                  
+SET SQL_SAFE_UPDATES = 0;
+
+delete from accounts 
+where branch_name in (select branch_name 
+					  from branch
+                      where branch_city='Bombay');
+
+SELECT * FROM LOAN ORDER BY AMOUNT DESC;
+
+CREATE VIEW BRANCH_TOTAL_LOAN (BRANCH_NAME, TOTAL_LOAN) AS SELECT BRANCH_NAME, SUM(AMOUNT) FROM LOAN GROUP BY BRANCH_NAME;
+
+UPDATE ACCOUNTS SET BALANCE=BALANCE *1.05; 
